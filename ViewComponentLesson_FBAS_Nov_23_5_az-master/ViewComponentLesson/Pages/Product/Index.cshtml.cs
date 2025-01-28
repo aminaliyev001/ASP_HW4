e3cc04556762737444d64eaf6c404d5ca448df2e
Amin Aliyev <aminaliyev001@gmail.com>
2025-01-28 22:51:23 +0400

    first commit

---
+++ ViewComponentLesson_FBAS_Nov_23_5_az-master/ViewComponentLesson/Pages/Product/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ViewComponentLesson.Pages.Product
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
